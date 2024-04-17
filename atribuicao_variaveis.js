515d74e142c592b5160537062aa0a0b8ddbf59ee
Davi <davidffelix@hotmail.com>
2024-04-17 16:22:19 -0300

    first commit

---
+++ atribuicao_variaveis.js
@@ -0,0 +1,19 @@
+console.log('Trabalhando com atribuição de variáveis');
+
+const idade = 32;
+const primeiroNome = 'David';
+const sobrenome = 'Felix';
+
+console.log(primeiroNome, sobrenome);
+console.log(primeiroNome + ' ' + sobrenome);
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+console.log("Meu nome é", primeiroNome, sobrenome);
+console.log('Meu nome é ' + primeiroNome + ' ' + sobrenome);
+
+const nomeCompleto = primeiroNome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+
+
+
